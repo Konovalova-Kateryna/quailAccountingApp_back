93a77b4db91a6d975ea7b492d7fc8185510ab3df
Konovalova-Kateryna <kytana2112@gmail.com>
2026-04-04 22:20:10 +0300

    fix validation bug

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,7 +2,7 @@ const {HttpError}=require("../utils");
 
 const validateBody=(schema)=>{
     const func=(req,res,next)=>{
-        const {error}=schema.validate(req.body);
+        const {error, value}=schema.validate(req.body);
         if(error){
              const message = error.details.map((d) => d.message).join("; ");
             return next(HttpError(400, message));
